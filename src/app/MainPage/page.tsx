17cf8eb2c15c6aa12a48ad60b3b32cd1134bad31
Yor-Jihons <ryo.malon814@gmail.com>
2025-10-04 19:05:25 +0900

    Added the table.

--- src/app/MainPage/page.tsx
+++ src/app/MainPage/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 //import viteLogo from '/vite.svg';
 import { useTranslation } from 'react-i18next';
 import '../../App.css';
+import styles from "./mainpage.module.css";
 import CommonLayout from '../layout';
 import Status from '../../types/Status';
 import Book from '../../types/Book';
@@ -48,6 +49,24 @@ function MainPage() {
           })}
         </select>
         <div>{i18n.t('menu.zoom_in')}</div>
+        <div className={styles.table1}>
+          <table className={styles.table1}>
+            <thead>
+              <tr><th className={styles.id}>ID</th>
+              <th className={styles.title}>タイトル</th>
+              <th className={styles.author}>著者</th>
+              <th className={styles.isbn}>ISBN</th></tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className={styles.id}>2024</td>
+                <td className={styles.title}>プログラミングが好きになる本～はじめてのプログラミング～</td>
+                <td className={styles.author}></td>
+                <td className={styles.isbn}>978-3-16-148410-0</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </CommonLayout>
   );
