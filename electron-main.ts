3d13390c25cd5ac0420cc7faeef14b869b1fe6b7
Yor-Jihons <ryo.malon814@gmail.com>
2025-10-08 20:49:03 +0900

    Added the IPC for the export-html.

--- electron-main.ts
+++ electron-main.ts
@@ -111,6 +111,11 @@ app.whenReady().then(() => {
     return (await dialog.showMessageBox( mainWindow, { message: message, buttons: buttons } ) as any).response;
   });
 
+  // TODO:
+  ipcMain.handle('export-html', () => {
+    return {success: true};
+  });
+
   ipcMain.handle('fetch-status', () => {
     return db.fetchStatus();
   });
